348eddf1e2cb2ab73e26acd75f7399d30a9f3495
Michael Alexander <beefsack@gmail.com>
2013-07-01 15:53:58 +1000

    Implemented skeleton and big swag of tests for tic-tac-toe

---
+++ game/tic_tac_toe/tic_tac_toe.go
@@ -0,0 +1,80 @@
+package tic_tac_toe
+
+import (
+	"errors"
+	"math/rand"
+	"regexp"
+)
+
+type Game struct {
+	Players         []string
+	CurrentlyMoving []string
+	StartPlayer     string
+	Board           [3][3]int // 0 = empty cell, 1 = first player, 2 = second player
+}
+
+// Create a new game for specified players
+func New(players []string) (error, Game) {
+	if len(players) != 2 {
+		return errors.New("Must be 2 players"), Game{}
+	}
+	startPlayer := players[rand.Int()%2]
+	return nil, Game{
+		Players:         players,
+		StartPlayer:     startPlayer,
+		CurrentlyMoving: []string{startPlayer},
+	}
+}
+
+// Make an action for the specified player
+func (g *Game) PlayerAction(player string, action string, args []string) error {
+	if g.CurrentlyMoving[0] != player {
+		return errors.New("Not your turn")
+	}
+	if !regexp.MustCompile("^[abcdefghi]$").MatchString(action) {
+		return errors.New("Your action must be a letter between a - i")
+	}
+	// @todo Convert a-i to cell coordinates (both 0-2) and call
+	// MarkCellForPlayer
+	g.NextPlayer()
+	return nil
+}
+
+func (g *Game) NextPlayer() {
+	// @todo Flip g.CurrentlyMoving[0] to the other player
+}
+
+// Marks the specified cell for the current player and changes the currently
+// moving player to the next one.  It shouldn't let you mark a cell that's
+// already marked.
+func (g *Game) MarkCellForPlayer(player string, x, y int) error {
+	// @todo implement
+	return errors.New("Not implemented yet")
+}
+
+// Render an ascii representation of the game for a player
+func (g *Game) RenderForPlayer(player string) (error, string) {
+	output := "This is an example\n"
+	output += "of some constructed output"
+	// @todo implement.
+	return errors.New("Not implemented yet"), output
+}
+
+// Check if there is a winner, if there is a line of 3 all 1s or 2s.  First
+// argument is false if there isn't a winner yet
+func (g *Game) CheckWinner() (bool, string) {
+	// @todo implement
+	return false, ""
+}
+
+// Check if the game is finished, i.e. if there is a winner or if there is no
+// empty cells
+func (g *Game) IsFinished() bool {
+	won, _ := g.CheckWinner()
+	if won {
+		return true
+	}
+	// @todo check if there are any empty cells and return false if there are
+	// any, otherwise return true
+	return false
+}
---
+++ game/tic_tac_toe/tic_tac_toe_test.go
@@ -0,0 +1,159 @@
+package tic_tac_toe
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	players := []string{"Mick", "Steve"}
+	err, _ := New(players)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestNewErrorsWithIncorrectPlayers(t *testing.T) {
+	players := []string{"Mick"}
+	err, _ := New(players)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestRenderForPlayer(t *testing.T) {
+	players := []string{"Mick", "Steve"}
+	err, game := New(players)
+	if err != nil {
+		t.Error(err)
+	}
+	err, _ = game.RenderForPlayer("Mick")
+}
+
+func TestPlayerAction(t *testing.T) {
+	players := []string{"Mick", "Steve"}
+	err, game := New(players)
+	if err != nil {
+		t.Error(err)
+	}
+	// First lets see that a valid action works
+	err = game.PlayerAction(game.CurrentlyMoving[0], "a", []string{})
+	if err != nil {
+		t.Error(err)
+	}
+	if game.Board[0][0] == 0 {
+		t.Error("The action didn't actually do anything")
+	}
+	// Now lets make an invalid action
+	err = game.PlayerAction(game.CurrentlyMoving[0], "moog", []string{})
+	if err == nil {
+		t.Error("It didn't actually error")
+	}
+}
+
+func TestNextPlayer(t *testing.T) {
+	players := []string{"Mick", "Steve"}
+	err, game := New(players)
+	if err != nil {
+		t.Error(err)
+	}
+	// Force the CurrentlyMoving[0] to what we want for testing sake
+	game.CurrentlyMoving[0] = "Mick"
+	game.NextPlayer()
+	if game.CurrentlyMoving[0] != "Steve" {
+		t.Error("Player didn't change to Steve")
+	}
+	game.NextPlayer()
+	if game.CurrentlyMoving[0] != "Mick" {
+		t.Error("Player didn't change back to Mick")
+	}
+}
+
+func TestMarkCellForPlayer(t *testing.T) {
+	players := []string{"Mick", "Steve"}
+	err, game := New(players)
+	if err != nil {
+		t.Error(err)
+	}
+	// First lets just mark a cell and see if it worked
+	err = game.MarkCellForPlayer("Mick", 1, 1)
+	if err != nil {
+		t.Error(err)
+	}
+	if game.Board[1][1] != 1 {
+		t.Error("Didn't mark cell for Mick")
+	}
+	// Lets mark a different cell with the other player to see that works too
+	err = game.MarkCellForPlayer("Steve", 1, 2)
+	if err != nil {
+		t.Error(err)
+	}
+	if game.Board[1][2] != 2 {
+		t.Error("Didn't mark cell for Steve")
+	}
+	// Now lets try to remark a cell that's already marked and expect an error
+	err = game.MarkCellForPlayer("Steve", 1, 1)
+	if err == nil {
+		t.Error("It let us change a cell that was already marked")
+	}
+}
+
+func TestCheckWinner(t *testing.T) {
+	players := []string{"Mick", "Steve"}
+	err, game := New(players)
+	if err != nil {
+		t.Error(err)
+	}
+	// This should be a winner for Mick
+	game.Board[0] = [3]int{1, 1, 2}
+	game.Board[1] = [3]int{1, 2, 1}
+	game.Board[2] = [3]int{1, 1, 2}
+	won, winner := game.CheckWinner()
+	if !won || winner != "Mick" {
+		t.Error("Winner isn't Mick")
+	}
+	// This should be a winner for Steve
+	game.Board[0] = [3]int{2, 1, 2}
+	game.Board[1] = [3]int{1, 2, 1}
+	game.Board[2] = [3]int{1, 1, 2}
+	won, winner = game.CheckWinner()
+	if !won || winner != "Steve" {
+		t.Error("Winner isn't Steve")
+	}
+	// This should be a winner for nobody
+	game.Board[0] = [3]int{1, 1, 2}
+	game.Board[1] = [3]int{2, 2, 1}
+	game.Board[2] = [3]int{1, 1, 2}
+	won, winner = game.CheckWinner()
+	if won {
+		t.Error("There's a winner when there shouldn't be")
+	}
+}
+
+func TestIsFinished(t *testing.T) {
+	players := []string{"Mick", "Steve"}
+	err, game := New(players)
+	if err != nil {
+		t.Error(err)
+	}
+	// This shouldn't be finished
+	game.Board[0] = [3]int{1, 1, 2}
+	game.Board[1] = [3]int{0, 0, 1}
+	game.Board[2] = [3]int{1, 1, 2}
+	if game.IsFinished() {
+		t.Error("Game shouldn't be finished")
+	}
+	// This should finished because all cells are full
+	game.Board[0] = [3]int{1, 1, 2}
+	game.Board[1] = [3]int{2, 2, 1}
+	game.Board[2] = [3]int{1, 1, 2}
+	if !game.IsFinished() {
+		t.Error("Game should be finished because all cells are full")
+	}
+	// This should finished because all cells are full
+	game.Board[0] = [3]int{0, 0, 1}
+	game.Board[1] = [3]int{0, 1, 0}
+	game.Board[2] = [3]int{1, 0, 0}
+	if !game.IsFinished() {
+		t.Error("Game should be finished because Mick won")
+	}
+}
