d01109519dbc5a6f10b223285a3c035c2ed6ee9b
Steve Taylor <baconheist@gmail.com>
2013-07-02 16:38:01 +1000

    added Player action switch

--- game/tic_tac_toe/tic_tac_toe.go
+++ game/tic_tac_toe/tic_tac_toe.go
@@ -35,6 +35,28 @@ func (g Game) PlayerAction(player string, action string, args []string) error {
 	if !regexp.MustCompile("^[abcdefghi]$").MatchString(action) {
 		return errors.New("Your action must be a letter between a - i")
 	}
+	switch action {
+	case a:
+		MarkCellForPlayer(player, 0, 0)
+	case b:
+		MarkCellForPlayer(player, 1, 0)
+	case c:
+		MarkCellForPlayer(player, 2, 0)
+	case d:
+		MarkCellForPlayer(player, 0, 1)
+	case e:
+		MarkCellForPlayer(player, 1, 1)
+	case f:
+		MarkCellForPlayer(player, 2, 1)
+	case g:
+		MarkCellForPlayer(player, 0, 2)
+	case h:
+		MarkCellForPlayer(player, 1, 2)
+	case i:
+		MarkCellForPlayer(player, 2, 2)
+	default:
+		fmt.Println(j, "how did this get here...")
+	}
 	// @todo Convert a-i to cell coordinates (both 0-2) and call
 	// MarkCellForPlayer
 	g.NextPlayer()
