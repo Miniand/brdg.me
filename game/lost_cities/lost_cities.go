b984b853a9f4e0a83233cd6ae8ca2c593dc1947e
Michael Alexander <beefsack@gmail.com>
2013-07-18 14:45:04 +1000

    More tests for lost cities.

--- game/lost_cities/lost_cities.go
+++ game/lost_cities/lost_cities.go
@@ -4,30 +4,28 @@ import (
 	"encoding/json"
 	"errors"
 	"math/rand"
+	"strings"
 	"time"
 )
 
-// Suit constants, iota numbers them incrementally from 0, so they are 0-4
-const (
-	RED = iota
-	GREEN
-	BLUE
-	WHITE
-	YELLOW
-)
-
-// A card is a suit and a value
-type Card struct {
-	// Suits are defined by the colour consts above 0-4
-	Suit int
-	// The value is 2-10 for cards an 0 for multiplier
-	Value int
-}
-
-// A hand is cards sorted into suits.  Each player has a hand, and the centre
-// discard piles are also stored as a hand.
-type Hand struct {
-	Suits [5][]Card
+type Game struct {
+	// Player 1 is at index 0, player 2 is at index 1
+	Players []string
+	// Currently moving player, stored as an int to make it easier to reference
+	// hands and scores
+	CurrentlyMoving int
+	// The current phase
+	TurnPhase int
+	// The board contains the game state of the current round
+	Board Board
+	// Round out of 3
+	Round int
+	// Round scores are a multidimensional array, by two players then by three
+	// rounds
+	RoundScores [2][3]int
+	// Tracks which players are ready for the next round before starting the
+	// next round, to give players a chance to see the board after a round
+	ReadyPlayers [2]bool
 }
 
 // The board consists of two players hands, a discard hand, and a draw pile
@@ -42,20 +40,36 @@ type Board struct {
 	DrawPile []Card
 }
 
-type Game struct {
-	// Player 1 is at index 0, player 2 is at index 1
-	Players []string
-	// Currently moving player, stored as an int to make it easier to reference
-	// hands and scores
-	CurrentlyMoving int
-	// The board contains the game state of the current round
-	Board Board
-	// Round out of 3
-	Round int
-	// Round scores are a multidimensional array, by two players then by three
-	// rounds
-	RoundScores [2][3]int
+// A hand is cards sorted into suits.  Each player has a hand, and the centre
+// discard piles are also stored as a hand.
+type Hand struct {
+	CardsBySuit [5][]Card
 }
+
+// A card is a suit and a value
+type Card struct {
+	// Suits are defined by the colour consts above 0-4
+	Suit int
+	// The value is 2-10 for cards an 0 for multiplier
+	Value int
+}
+
+// Turn phase constant
+const (
+	// The first half of the turn, playing or discarding a card
+	TURN_PHASE_PLAY_OR_DISCARD = iota
+	// The second half of the turn, drawing a card from discards or the deck
+	TURN_PHASE_DRAW
+)
+
+// Suit constants, iota numbers them incrementally from 0, so they are 0-4
+const (
+	SUIT_RED = iota
+	SUIT_GREEN
+	SUIT_BLUE
+	SUIT_WHITE
+	SUIT_YELLOW
+)
 
 func (g *Game) Start(players []string) error {
 	if len(players) != 2 {
@@ -63,12 +77,54 @@ func (g *Game) Start(players []string) error {
 	}
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	g.Players = players
+	err := g.InitRound()
+	if err != nil {
+		return err
+	}
 	g.CurrentlyMoving = r.Int() % 2
 	return nil
 }
 
-func (g *Game) PlayerAction(player, action string, params []string) error {
+// Shuffle cards and deal hands, set the start player, set the turn phase etc
+func (g *Game) InitRound() error {
 	return nil
+}
+
+// Gets the player number, given a string name
+func (g *Game) PlayerFromString(player string) (int, error) {
+	for key, name := range g.Players {
+		if name == player {
+			return key, nil
+		}
+	}
+	return 0, errors.New("Couldn't find player with name: " + player)
+}
+
+// Takes a string like b6, rx, y10 and turns it into a Card object
+func (g *Game) ParseCardString(cardString string) (Card, error) {
+	return Card{}, nil
+}
+
+func (g *Game) PlayerAction(player, action string, params []string) error {
+	playerNum, err := g.PlayerFromString(player)
+	if err != nil {
+		return err
+	}
+	switch strings.ToLower(action) {
+	case "play":
+		err = g.PlayCard(playerNum, Card{})
+	case "discard":
+		err = g.DiscardCard(playerNum, Card{})
+	case "take":
+		err = g.TakeCard(playerNum, SUIT_RED)
+	case "draw":
+		err = g.DrawCard(playerNum)
+	case "ready":
+		err = g.PlayerReady(playerNum)
+	default:
+		err = errors.New("Did not understand your action: " + action)
+	}
+	return err
 }
 
 func (g *Game) Name() string {
@@ -105,4 +161,46 @@ func (g *Game) Winners() []string {
 
 func (g *Game) WhoseTurn() []string {
 	return []string{}
+}
+
+// Returns the full set of cards in a game, 3 investment cards and 9 point cards
+// for each expedition totalling 60 cards
+func (g *Game) AllCards() []Card {
+	return []Card{}
+}
+
+// Play a card from the hand into an expedition, checking that it is the
+// player's turn, that they have the card in their hand, and that they are able
+// to play the card.  Return an error if any of these don't pass.
+func (g *Game) PlayCard(player int, card Card) error {
+	return nil
+}
+
+// Take a card from discard stacks into the hand, checking that it is the
+// player's turn, and that the discard stack has cards in it.  Return an error
+// if any of these don't pass.
+func (g *Game) TakeCard(player int, suit int) error {
+	return nil
+}
+
+// Play a card from the hand into an expedition, checking that it is the
+// player's turn, that they have the card in their hand, and that they are able
+// to play the card.  Return an error if any of these don't pass.
+func (g *Game) DrawCard(player int) error {
+	return nil
+}
+
+// Play a card from the hand into an expedition, checking that it is the
+// player's turn, that they have the card in their hand, and that they are able
+// to play the card.  Return an error if any of these don't pass.
+func (g *Game) DiscardCard(player int, card Card) error {
+	return nil
+}
+
+// Mark a player as ready for the next round, gives them a chance to see the
+// board and scores after the round has ended.  Check that it is actually the
+// end of the round and that it isn't the last round already, returning an error
+// if either of those fail.
+func (g *Game) PlayerReady(player int) error {
+	return nil
 }
--- game/lost_cities/lost_cities_test.go
+++ game/lost_cities/lost_cities_test.go
@@ -4,17 +4,49 @@ import (
 	"testing"
 )
 
-func TestStart(t *testing.T) {
+// Build a game by hand for testing purposes.  Each player has a full hand, half
+// of the discard stacks have cards, and there are two cards in the draw pile.
+func mockGame() *Game {
 	players := []string{"Mick", "Steve"}
 	game := &Game{}
 	err := game.Start(players)
 	if err != nil {
-		t.Error(err)
+		panic("Unable to start the game: " + err.Error())
 	}
+	return game
+}
+
+func TestStart(t *testing.T) {
+	game := mockGame()
 	if game.PlayerList()[0] != "Mick" {
 		t.Error("Mick is not a player")
 	}
 	if game.PlayerList()[1] != "Steve" {
 		t.Error("Steve is not a player")
+	}
+	if len(game.Board.PlayerHands[0].CardsBySuit[SUIT_RED])+
+		len(game.Board.PlayerHands[0].CardsBySuit[SUIT_GREEN])+
+		len(game.Board.PlayerHands[0].CardsBySuit[SUIT_BLUE])+
+		len(game.Board.PlayerHands[0].CardsBySuit[SUIT_WHITE])+
+		len(game.Board.PlayerHands[0].CardsBySuit[SUIT_YELLOW]) != 8 {
+		t.Error("Mick's hand isn't 8 cards in total")
+	}
+	if len(game.Board.PlayerHands[1].CardsBySuit[SUIT_RED])+
+		len(game.Board.PlayerHands[1].CardsBySuit[SUIT_GREEN])+
+		len(game.Board.PlayerHands[1].CardsBySuit[SUIT_BLUE])+
+		len(game.Board.PlayerHands[1].CardsBySuit[SUIT_WHITE])+
+		len(game.Board.PlayerHands[1].CardsBySuit[SUIT_YELLOW]) != 8 {
+		t.Error("Steve's hand isn't 8 cards in total")
+	}
+	if game.IsFinished() {
+		t.Error("Game is already finished but we just started!")
+	}
+}
+
+func TestAllCards(t *testing.T) {
+	game := mockGame()
+	allCards := game.AllCards()
+	if len(allCards) != 60 {
+		t.Error("There aren't 60 cards in the full deck")
 	}
 }
