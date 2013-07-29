de9ac46e3d1b04d8aa784a5d3678fd022db9841f
Michael Alexander <beefsack@gmail.com>
2013-07-30 00:13:01 +1000

    Replicated first player fold issue in test.

--- game/texas_holdem/texas_holdem_test.go
+++ game/texas_holdem/texas_holdem_test.go
@@ -19,3 +19,28 @@ func TestStart(t *testing.T) {
 		t.Fatal("Didn't set players")
 	}
 }
+
+func TestNextPhaseOnInitialFold(t *testing.T) {
+	g := &Game{}
+	err := g.Start([]string{"Mick", "Steve", "BJ"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	// First player folds
+	err = g.PlayerAction(g.Players[g.CurrentPlayer], "fold", []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Next two players call and check, should flop
+	err = g.PlayerAction(g.Players[g.CurrentPlayer], "call", []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = g.PlayerAction(g.Players[g.CurrentPlayer], "check", []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(g.CommunityCards) != 3 {
+		t.Fatal("No flop, community cards:", g.CommunityCards)
+	}
+}
