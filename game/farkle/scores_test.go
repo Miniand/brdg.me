43e0e122708e627e264a1a56ae325e52b4f75edf
Michael Alexander <beefsack@gmail.com>
2013-08-16 00:20:51 +1000

    Added tests for dice and string converters, refs #28

--- game/farkle/scores_test.go
+++ game/farkle/scores_test.go
@@ -24,3 +24,32 @@ func TestDiceInDice(t *testing.T) {
 		t.Fatal("Expected to not be in")
 	}
 }
+
+func TestDiceToScoreString(t *testing.T) {
+	str, err := DiceToScoreString([]int{1, 2, 3, 4})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if str != "1234" {
+		t.Fatal("Expected string to be 1234, got:", str)
+	}
+	str, err = DiceToScoreString([]int{0})
+	if err == nil {
+		t.Fatal("Expected an error")
+	}
+	str, err = DiceToScoreString([]int{7})
+	if err == nil {
+		t.Fatal("Expected an error")
+	}
+}
+
+func TestScoreStringToDice(t *testing.T) {
+	dice, err := ScoreStringToDice("11153")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect := []int{1, 1, 1, 5, 3}
+	if !DiceEquals(dice, expect) {
+		t.Fatal("Expected dice to be", expect, "but got", dice)
+	}
+}
