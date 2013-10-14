782d25cfd7c6d3458ed03f79a2e32da6b3142ab9
Michael Alexander <beefsack@gmail.com>
2013-10-15 00:19:10 +1100

    Added placeholder for active games.

--- server/web/view/header.go
+++ server/web/view/header.go
@@ -37,7 +37,10 @@ header {
 }
 
 header .logo {
-	float: left;
+	position: absolute;
+	left: 2em;
+	top: 2em;
+	height: 4em;
 	text-align: left;
 	display: inline-block;
 	white-space: pre-wrap;
@@ -50,7 +53,10 @@ header .logo a {
 }
 
 header .account {
-	float: right;
+	position: absolute;
+	right: 2em;
+	top: 1em;
+	height: 4em;
 	line-height: 4em;
 }
 
@@ -66,11 +72,66 @@ header .header-end {
 .game-list .game {
 	text-align: center;
 }
+
+.active-games {
+	margin-left: 14em;
+	margin-right: 14em;
+	margin-top: 0.5em;
+	white-space: nowrap;
+	overflow-x: auto;
+}
+
+.active-game {
+	display: inline-block;
+	white-space: normal;
+	line-height: 1.5em;
+	border: 1px dashed gray;
+	padding: 0 1em;
+	margin-bottom: 0.5em;
+}
+
+.active-game-your-turn {
+	font-weight: bold;
+	border: 1px solid gray;
+	background-color: silver;
+}
+
+.active-game-name {
+	text-align: center;
+}
+
+.active-game-player {
+	text-align: center;
+}
 </style>
 <title>brdg.me - {{.}}</title>
 <body>
 <header>
 	<div class="logo"><a href="/">{{template "title"}}</a></div>
+	<div class="active-games">
+		<a href="/" class="active-game active-game-your-turn">
+			<div class="active-game-name">Texas hold 'em (183f94ca)</div>
+			<div class="active-game-player">Your turn</div>
+		</a> <a href="/" class="active-game active-game-your-turn">
+			<div class="active-game-name">Texas hold 'em (183f94ca)</div>
+			<div class="active-game-player">Your turn</div>
+		</a> <a href="/" class="active-game">
+			<div class="active-game-name">Texas hold 'em (183f94ca)</div>
+			<div class="active-game-player">baconheist's turn</div>
+		</a> <a href="/" class="active-game">
+			<div class="active-game-name">Texas hold 'em (183f94ca)</div>
+			<div class="active-game-player">striker203 and ashtermet's turn</div>
+		</a> <a href="/" class="active-game">
+			<div class="active-game-name">Texas hold 'em (183f94ca)</div>
+			<div class="active-game-player">baconheist's turn</div>
+		</a> <a href="/" class="active-game">
+			<div class="active-game-name">Texas hold 'em (183f94ca)</div>
+			<div class="active-game-player">baconheist's turn</div>
+		</a> <a href="/" class="active-game">
+			<div class="active-game-name">Texas hold 'em (183f94ca)</div>
+			<div class="active-game-player">baconheist's turn</div>
+		</a>
+	</div>
 	<div class="account">
 		<a href="/sign-in">Sign in / register</a>
 	</div>
