48824f4662e7e2aa349c0fb0b697c6e5e688191a
Michael Alexander <beefsack@gmail.com>
2014-04-06 21:13:29 +1000

    Implemented basic canvas.

--- render/canvas.go
+++ render/canvas.go
@@ -1,11 +1,96 @@
 package render
 
+import (
+	"bytes"
+	"regexp"
+	"strings"
+)
+
+var pixelRegexp = regexp.MustCompile(`^((?:\{\{.+?\}\})*)(.)((?:\{\{_.+?\}\})*)`)
+
+// Canvas is a two dimensional plane to which you can write strings by location.
 type Canvas struct {
+	pixels map[int]map[int]string
 }
 
+// NewCanvas initialises a Canvas struct.
+func NewCanvas() *Canvas {
+	return &Canvas{
+		pixels: map[int]map[int]string{},
+	}
+}
+
+func readPixel(input string) (pixel, prefix, suffix, remaining string) {
+	if input == "" {
+		return "", "", "", ""
+	}
+	matches := pixelRegexp.FindStringSubmatch(input)
+	if matches == nil {
+		return "", "", "", ""
+	}
+	return matches[2], matches[1], matches[3], input[len(matches[0]):]
+}
+
+// Draw draws a string to the canvas at the specified location.
 func (c *Canvas) Draw(x, y int, content string) {
+	xPtr := x
+	yPtr := y
+	remaining := content
+	for len(remaining) > 0 {
+		var pixel, prefix, suffix string
+		pixel, prefix, suffix, remaining = readPixel(remaining)
+		if c.pixels[yPtr] == nil {
+			c.pixels[yPtr] = map[int]string{}
+		}
+		c.pixels[yPtr][xPtr] = prefix + pixel + suffix
+		xPtr += 1
+	}
 }
 
+// Render render all of the drawn objects in the smallest possible rectangle.
+// Render calculates the smallest rectangle containing all drawn objects and
+// defers to RenderRect.
 func (c *Canvas) Render() string {
-	return ""
+	var minX, minY, maxX, maxY int
+	firstPass := true
+	for y, row := range c.pixels {
+		if firstPass || y < minY {
+			minY = y
+		}
+		if firstPass || y > maxY {
+			maxY = y
+		}
+		for x, _ := range row {
+			if firstPass || x < minX {
+				minX = x
+			}
+			if firstPass || x > maxX {
+				maxX = x
+			}
+			firstPass = false
+		}
+		firstPass = false
+	}
+	return c.RenderRect(minX, minY, maxX-minX+1, maxY-minY+1)
+}
+
+// RenderRect renders all of the drawn objects given the passed rectangle.
+func (c *Canvas) RenderRect(x, y, width, height int) string {
+	lines := []string{}
+	for yPtr := y; yPtr < y+height; yPtr++ {
+		if c.pixels[yPtr] == nil {
+			lines = append(lines, strings.Repeat(" ", width))
+			continue
+		}
+		line := bytes.Buffer{}
+		for xPtr := x; xPtr < x+width; xPtr++ {
+			pixel := c.pixels[yPtr][xPtr]
+			if pixel == "" {
+				pixel = " "
+			}
+			line.WriteString(pixel)
+		}
+		lines = append(lines, line.String())
+	}
+	return strings.Join(lines, "\n")
 }
--- render/canvas_test.go
+++ render/canvas_test.go
@@ -1,11 +1,65 @@
 package render
 
-func ExampleCanvasRenderTagPositioning() {
-	c := &Canvas{}
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReadPixel(t *testing.T) {
+	cases := map[string][]string{
+		"blah":                           []string{"b", "", "", "lah"},
+		`{{c "red"}}YA{{_c}}`:            []string{"Y", `{{c "red"}}`, "", "A{{_c}}"},
+		`{{c "red"}}{{b}}YA{{_b}}{{_c}}`: []string{"Y", `{{c "red"}}{{b}}`, "", "A{{_b}}{{_c}}"},
+		`{{c "red"}}{{b}}Y{{_b}}{{_c}}`:  []string{"Y", `{{c "red"}}{{b}}`, `{{_b}}{{_c}}`, ""},
+		`{{c "red"}}Y{{b}}A{{_b}}{{_c}}`: []string{"Y", `{{c "red"}}`, "", "{{b}}A{{_b}}{{_c}}"},
+	}
+	for input, expected := range cases {
+		actualPixel, actualPrefix, actualSuffix, actualRemaining := readPixel(input)
+		if expected[0] != actualPixel {
+			t.Errorf("For %s, expected pixel to be %s but got %s",
+				input, expected[0], actualPixel)
+		}
+		if expected[1] != actualPrefix {
+			t.Errorf("For %s, expected prefix to be %s but got %s",
+				input, expected[1], actualPrefix)
+		}
+		if expected[2] != actualSuffix {
+			t.Errorf("For %s, expected suffix to be %s but got %s",
+				input, expected[2], actualSuffix)
+		}
+		if expected[3] != actualRemaining {
+			t.Errorf("For %s, expected remaining to be %s but got %s",
+				input, expected[3], actualRemaining)
+		}
+	}
+}
+
+func ExampleCanvas_Render_positioning() {
+	c := NewCanvas()
+	c.Draw(-2, -4, "Hello...")
+	c.Draw(1, -3, "World")
+	fmt.Println(c.Render())
+	// Output:
+	// Hello...
+	//    World
+}
+
+func ExampleCanvas_Render_tags() {
+	c := NewCanvas()
 	c.Draw(-1, -3, `{{b}}1{{_b}}`)
 	c.Draw(1, -3, `{{c "green"}}2{{_c}}`)
 	c.Draw(0, -2, `3`)
+	fmt.Println(c.Render())
 	// Output:
 	// {{b}}1{{_b}} {{c "green"}}2{{_c}}
 	//  3
+}
+
+func ExampleCanvas_Render_overwrite() {
+	c := NewCanvas()
+	c.Draw(-2, -4, `{{b}}Egg{{_b}}`)
+	c.Draw(-3, -4, `Bacon`)
+	fmt.Println(c.Render())
+	// Output:
+	// Bacon
 }
