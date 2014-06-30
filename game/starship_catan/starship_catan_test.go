0723187b919e68a1390722c749126ea532ecf3a0
Michael Alexander <beefsack@gmail.com>
2014-06-30 15:54:10 +1000

    Added test for choosing modules.

--- game/starship_catan/starship_catan_test.go
+++ game/starship_catan/starship_catan_test.go
@@ -1,10 +1,57 @@
 package starship_catan
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Miniand/brdg.me/command"
+)
+
+const (
+	Mick  = "Mick"
+	Steve = "Steve"
+)
 
 func TestStart(t *testing.T) {
 	g := &Game{}
-	if err := g.Start([]string{"mick", "steve"}); err != nil {
+	if err := g.Start([]string{Mick, Steve}); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestChooseModule(t *testing.T) {
+	g := &Game{}
+	if err := g.Start([]string{Mick, Steve}); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual([]string{Mick, Steve}, g.WhoseTurn()) {
+		t.Fatal("Expected it to be both Mick and Steve's turn.")
+	}
+	if _, err := command.CallInCommands(Steve, g,
+		"choose lo", g.Commands()); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(map[int]int{
+		ModuleLogistics: 1,
+	}, g.PlayerBoards[1].Modules) {
+		t.Fatal("Expected Steve to have a level 1 logistics module.")
+	}
+	if !reflect.DeepEqual([]string{Mick}, g.WhoseTurn()) {
+		t.Fatal("Expected it to be only Mick's turn.")
+	}
+	if _, err := command.CallInCommands(Mick, g,
+		"choose se", g.Commands()); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(map[int]int{
+		ModuleSensor: 1,
+	}, g.PlayerBoards[0].Modules) {
+		t.Fatal("Expected Mick to have a level 1 sensor module.")
+	}
+	if !reflect.DeepEqual([]string{Mick}, g.WhoseTurn()) {
+		t.Fatal("Expected it to be only Mick's turn.")
+	}
+	if g.Phase == PhaseChooseModule {
+		t.Fatal("It should no longer be the choose module phase.")
 	}
 }
