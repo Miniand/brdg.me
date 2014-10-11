260aec74fd3e8c427375d77e09b136854294eabe
Michael Alexander <beefsack@gmail.com>
2014-10-11 16:33:45 +1100

    Fixed regexp commands to force to start at start of command string, fixes acquire trade and sell bug.

--- command/helper.go
+++ command/helper.go
@@ -43,11 +43,11 @@ func ParseNamedCommand(name string, input string) []string {
 
 // Parses using provided regexp, replacing spaces with non-newline space matchers
 func ParseRegexp(reg, input string) []string {
-	return regexp.MustCompile(fmt.Sprintf(`(?im)^[^\S\r\n]*%s[^\S\r\n]*$`,
+	return regexp.MustCompile(fmt.Sprintf(`(?im)\A[^\S\r\n]*%s[^\S\r\n]*$`,
 		regexp.MustCompile(`\bARG\b`).ReplaceAllString(
 			regexp.MustCompile(`\s+?`).ReplaceAllString(reg, `[^\S\r\n]+`),
 			`\b[^\s]+\b`))).
-		FindStringSubmatch(input)
+		FindStringSubmatch(strings.TrimSpace(input))
 }
 
 // Extracts the actual arguments from the result of a ParseNamedCommand call
