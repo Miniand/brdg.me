fd43e68dbd14f2ffdf2475b7c133ccdf269356b5
Michael Alexander <beefsack@gmail.com>
2014-10-12 18:33:59 +1100

    Removed old web stuff.

--- server/web/controller/game.go
+++ server/web/controller/game.go
@@ -10,16 +10,10 @@ import (
 	"github.com/Miniand/brdg.me/render"
 	sgame "github.com/Miniand/brdg.me/server/game"
 	"github.com/Miniand/brdg.me/server/model"
-	"github.com/Miniand/brdg.me/server/web/view"
-	gameView "github.com/Miniand/brdg.me/server/web/view/game"
+
 	"github.com/dancannon/gorethink"
 	"github.com/gorilla/mux"
 )
-
-func GameIndex(w http.ResponseWriter, r *http.Request) {
-	view.LoggedInUser = GetEmail(r)
-	gameView.Index(w)
-}
 
 func ApiGameIndex(w http.ResponseWriter, r *http.Request) {
 	loggedIn, authUser := ApiMustAuthenticate(w, r)
@@ -115,52 +109,6 @@ func ApiGameShow(w http.ResponseWriter, r *http.Request) {
 		"log":        logHtml,
 		"commands":   commandHtml,
 	}, w, r)
-}
-
-func GameShow(w http.ResponseWriter, r *http.Request) {
-	view.LoggedInUser = GetEmail(r)
-	vars := mux.Vars(r)
-	gm, err := model.LoadGame(vars["id"])
-	if err != nil {
-		fmt.Println(err.Error())
-		http.Redirect(w, r, "/", http.StatusFound)
-	}
-	gameView.Show(w, gameView.ShowScope{
-		GameModel: gm,
-	})
-}
-
-func GameNew(w http.ResponseWriter, r *http.Request) {
-	view.LoggedInUser = GetEmail(r)
-	vars := mux.Vars(r)
-	g := game.RawCollection()[vars["identifier"]]
-	if g == nil {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-	gameView.New(w, gameView.NewScope{
-		Game:    g,
-		Players: r.URL.Query()["players[]"],
-	})
-}
-
-func GameCreate(w http.ResponseWriter, r *http.Request) {
-	view.LoggedInUser = GetEmail(r)
-	g := game.RawCollection()[r.PostFormValue("identifier")]
-	if g == nil {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-	players := r.PostForm["players[]"]
-	players = append(players, view.LoggedInUser)
-	gm, err := model.StartNewGame(g, players)
-	if err != nil {
-		fmt.Println(err.Error())
-		http.Redirect(w, r, "/", http.StatusFound)
-	} else {
-		fmt.Println(gm.Id)
-		http.Redirect(w, r, "/game/"+gm.Id, http.StatusFound)
-	}
 }
 
 func ApiGameCreate(w http.ResponseWriter, r *http.Request) {
--- server/web/route/route.go
+++ server/web/route/route.go
@@ -11,37 +11,21 @@ var router *mux.Router
 func Router() *mux.Router {
 	if router == nil {
 		router = mux.NewRouter()
-		router.HandleFunc("/", controller.Root).Methods("GET").Name("root")
-		router.HandleFunc("/session/sign-in", controller.SessionSignIn).
-			Methods("POST").Name("sessionSignIn")
-		router.HandleFunc("/session/sign-out", controller.SessionSignOut).
-			Methods("POST").Name("sessionSignOut")
-		router.HandleFunc("/", controller.Root).Methods("GET").
-			Name("sessionSignOut")
-		router.HandleFunc("/game", controller.GameIndex).Methods("GET").
-			Name("gameIndex")
-		router.HandleFunc("/game", controller.GameCreate).Methods("POST").
-			Name("gameCreate")
-		router.HandleFunc("/game/{id}", controller.GameShow).Methods("GET").
-			Name("gameShow")
-		router.HandleFunc("/game/new/{identifier}", controller.GameNew).
-			Methods("GET").Name("gameNew")
+		router.HandleFunc("/{url:.*}", controller.ApiOptions).Methods("OPTIONS").
+			Name("apiOptions")
+		router.HandleFunc("/game", controller.ApiGameIndex).Methods("GET").
+			Name("apiGameIndex")
+		router.HandleFunc("/game", controller.ApiGameCreate).Methods("POST").
+			Name("apiGameCreate")
+		router.HandleFunc("/game/{id}", controller.ApiGameShow).Methods("GET").
+			Name("apiGameShow")
+		router.HandleFunc("/game/{id}/command", controller.ApiGameCommand).Methods("POST").
+			Name("apiGameCommand")
 		router.HandleFunc("/ws", controller.Websocket)
 		router.HandleFunc("/auth/request", controller.AuthRequest).Methods("POST").
 			Name("authRequest")
 		router.HandleFunc("/auth/confirm", controller.AuthConfirm).Methods("POST").
 			Name("authConfirm")
-		api := router.PathPrefix("/api/").Subrouter()
-		api.HandleFunc("/{url:.*}", controller.ApiOptions).Methods("OPTIONS").
-			Name("apiOptions")
-		api.HandleFunc("/game", controller.ApiGameIndex).Methods("GET").
-			Name("apiGameIndex")
-		api.HandleFunc("/game", controller.ApiGameCreate).Methods("POST").
-			Name("apiGameCreate")
-		api.HandleFunc("/game/{id}", controller.ApiGameShow).Methods("GET").
-			Name("apiGameShow")
-		api.HandleFunc("/game/{id}/command", controller.ApiGameCommand).Methods("POST").
-			Name("apiGameCommand")
 	}
 	return router
 }
