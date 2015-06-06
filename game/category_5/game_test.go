e1ba1da2938295045f421c60a615501616cfff49
Michael Alexander <beefsack@gmail.com>
2015-06-06 20:03:05 +1000

    Fixed Miniand/brdg.me-issues#44 by implementing auto play of final cards in Category 5.

--- game/category_5/game_test.go
+++ game/category_5/game_test.go
@@ -3,6 +3,7 @@ package category_5
 import (
 	"testing"
 
+	"github.com/Miniand/brdg.me/game/helper"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,6 +16,24 @@ func TestGame_DrawCards(t *testing.T) {
 	assert.Len(t, g.DrawCards(10), 10)
 	assert.Len(t, g.Discard, 0)
 	assert.Len(t, g.Deck, 70)
+}
+
+func TestAutoPlayLastCard(t *testing.T) {
+	g := &Game{}
+	assert.NoError(t, g.Start(helper.Players[:2]))
+	g.Board = [4][]Card{
+		{1},
+		{2},
+		{3},
+		{4},
+	}
+	g.Hands = map[int][]Card{
+		helper.Mick:  {5, 6},
+		helper.Steve: {7, 8},
+	}
+	assert.NoError(t, helper.Cmd(g, helper.Mick, "play 5"))
+	assert.NoError(t, helper.Cmd(g, helper.Steve, "play 7"))
+	assert.Len(t, g.Hands[helper.Mick], 10)
 }
 
 func TestSortCards(t *testing.T) {
