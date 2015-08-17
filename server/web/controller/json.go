3e9d6fce84757accc5b071cce75f228df0845493
Michael Alexander <beefsack@gmail.com>
2015-08-17 12:09:13 +1000

    Removed debugging debug.PrintStack()

--- server/web/controller/json.go
+++ server/web/controller/json.go
@@ -3,13 +3,11 @@ package controller
 import (
 	"encoding/json"
 	"net/http"
-	"runtime/debug"
 )
 
 func Json(status int, data interface{}, w http.ResponseWriter, r *http.Request) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	WriteCorsHeaders(w, r)
 	w.WriteHeader(status)
-	debug.PrintStack()
 	return json.NewEncoder(w).Encode(data)
 }
