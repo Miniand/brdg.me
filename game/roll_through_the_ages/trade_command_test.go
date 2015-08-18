a62795ddf0781c42dc09307a762bf522e633209d
Michael Alexander <beefsack@gmail.com>
2015-08-18 13:30:34 +1000

    Fixed test to be more reliable

--- game/roll_through_the_ages/trade_command_test.go
+++ game/roll_through_the_ages/trade_command_test.go
@@ -11,17 +11,15 @@ func TestTradeCommand(t *testing.T) {
 	g := &Game{}
 	g.Start(helper.Players[:3])
 	g.CurrentPlayer = helper.Mick
-	g.Boards[helper.Mick].Developments[DevelopmentEngineering] = true
-	g.Boards[helper.Mick].Goods[GoodStone] = 3
+	g.Phase = PhaseRoll
 	g.RolledDice = []int{
 		DiceFood,
 		DiceFood,
 		DiceFood,
-		DiceFood,
-		DiceFood,
-		DiceFood,
-		DiceFood,
 	}
+	g.KeptDice = []int{}
+	g.Boards[helper.Mick].Developments[DevelopmentEngineering] = true
+	g.Boards[helper.Mick].Goods[GoodStone] = 3
 	assert.NoError(t, helper.Cmd(g, helper.Mick, "next"))
 	assert.NoError(t, helper.Cmd(g, helper.Mick, "trade 3"))
 	assert.Equal(t, 0, g.Boards[helper.Mick].Goods[GoodStone])
