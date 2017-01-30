4e409c87b64705a0e311fbadf120a10f5b4f34aa
Michael Alexander <beefsack@gmail.com>
2017-01-30 12:00:32 +1100

    Fixed Age of War winners being incorrect

--- game/age_of_war/game_test.go
+++ game/age_of_war/game_test.go
@@ -1,6 +1,8 @@
 package age_of_war
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/Miniand/brdg.me/game/helper"
@@ -10,4 +12,14 @@ import (
 func TestGame_Start(t *testing.T) {
 	g := &Game{}
 	assert.NoError(t, g.Start(helper.Players[:3]))
+}
+
+func TestGame_IsFinished(t *testing.T) {
+	g := &Game{}
+	f, err := os.Open("testdata/1e3f5cd4-ee30-4bc7-8f76-36c3fdc55476")
+	assert.NoError(t, err)
+	data, err := ioutil.ReadAll(f)
+	assert.NoError(t, err)
+	assert.NoError(t, g.Decode(data))
+	assert.Equal(t, []string{"baconheist@gmail.com"}, g.Winners())
 }
